8edc33a787829b825e5bc4838d332499bdcd80e7
kowlco <ptoxas@gmail.com>
2022-04-18 02:03:35 +0300

    createUserTable impl

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,16 +1,35 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    public UserDaoJDBCImpl() {
+    Connection connection;
 
+    public UserDaoJDBCImpl() {
+        try {
+            connection = Util.getSQLConnection();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void createUsersTable() {
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "CREATE TABLE `schema`.`test_table` (`id` INT, `name` VARCHAR(45), `lastname` VARCHAR(45), `age` INT);";
 
+            statement.execute(SQL);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
