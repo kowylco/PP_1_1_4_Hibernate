c22aefdac893bf93c7d1766eb56b73595acceb65
kowlco <ptoxas@gmail.com>
2022-04-18 01:02:46 +0300

    Connection to db

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,17 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/mysql?serverTimezone=UTC";
+    private static final String USERNAME = "kata";
+    private static final String PASSWORD = "P@ssw0rd";
+
+    public static Connection getSQLConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        return DriverManager.getConnection(DB_URL,USERNAME, PASSWORD);
+    }
 }
