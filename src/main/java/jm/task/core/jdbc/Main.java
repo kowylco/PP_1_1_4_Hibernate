216a1a91471294ed73e698710696a092000393b0
kowlco <ptoxas@gmail.com>
2022-04-18 04:26:02 +0300

    Main по заданию

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -8,27 +8,21 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
         UserDao userDao = new UserDaoJDBCImpl();
+
+//1 Создание таблицы User(ов)
         userDao.createUsersTable();
-        userDao.createUsersTable();
-
-        userDao.saveUser("Aasya", "Pukin", (byte) 23);
-        userDao.saveUser("Xasya", "Pukin", (byte) 34);
-        userDao.saveUser("FEsya", "Pukin", (byte) 24);
-
-        userDao.removeUserById(1);
-
+//2 Добавление 4 User(ов) в таблицу с данными на свой выбор. После каждого добавления должен быть вывод в консоль
+        userDao.saveUser("Vasya", "Lozhkin", (byte) 23);
+        userDao.saveUser("Petya", "Vilkin", (byte) 34);
+        userDao.saveUser("Zosya", "Golubikovaya", (byte) 24);
+        userDao.saveUser("Ernest", "Goloveshkin", (byte) 56);
+//3 Получение всех User из базы и вывод в консоль
         List<User> users = userDao.getAllUsers();
         users.forEach(System.out::println);
-
-        System.out.println("after clean");
+//4 Очистка таблицы User(ов)
         userDao.cleanUsersTable();
-        users = userDao.getAllUsers();
-        users.forEach(System.out::println);
-
-
-        userDao.dropUsersTable();
+//5 Удаление таблицы
         userDao.dropUsersTable();
     }
 }
