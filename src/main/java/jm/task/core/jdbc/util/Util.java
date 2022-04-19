15f16ca22bda3eb067a55464d0c9ba4d49d31c47
kowlco <ptoxas@gmail.com>
2022-04-20 01:36:29 +0300

    add hib settings

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,12 +1,20 @@
 package jm.task.core.jdbc.util;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     // реализуйте настройку соеденения с БД
     private static Connection connection;
+    private static SessionFactory sessionFactory;
 
     private static final String DB_URL = "jdbc:mysql://localhost:3306/schema?serverTimezone=UTC";
     private static final String USERNAME = "kata";
@@ -22,5 +30,32 @@ public class Util {
             connection = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
         }
         return connection;
+    }
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+                Properties properties = new Properties();
+
+                properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                properties.put(Environment.URL, DB_URL);
+                properties.put(Environment.USER, USERNAME);
+                properties.put(Environment.PASS, PASSWORD);
+                properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+                properties.put(Environment.SHOW_SQL, "true");
+                properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+                configuration.setProperties(properties);
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties())
+                        .build();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+        return sessionFactory;
     }
 }
